559a9fbc95ae5bddef3fcbdf32a3af69cbb71312
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-22 17:25:19 +0600

    feat: Code organized

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+// mongdb url
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ffrq.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -21,21 +22,25 @@ async function run() {
     try {
         await client.connect();
         const coffeesCollection = client.db('coffeesDB').collection('coffees');
+        // all coffees data
         app.get('/coffees', async (req, res) => {
             const result = await coffeesCollection.find().toArray();
             res.send(result)
         })
+        // single coffee data
         app.get('/coffees/:id', async (req, res) => {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)};
             const result = await coffeesCollection.findOne(query);
             res.send(result)
         })
+        // add coffee data in server
         app.post('/coffees', async (req, res) => {
             const data = req.body;
             const result = await coffeesCollection.insertOne(data);
             res.send(result);
         })
+        // edir coffee data in server
         app.put('/coffees/:id', async (req, res) => {
             const data = req.body;
             const id = req.params.id;
@@ -56,6 +61,7 @@ async function run() {
             const result = await coffeesCollection.updateOne(filter, updateCoffee, options);
             res.send(result);
         })
+        // single coffee data delete in server
         app.delete('/coffees/:id', async (req, res) => {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)};
