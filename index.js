c4820749b76f6d489f55589e6e0ec9b3a8d517d1
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-22 12:17:15 +0600

    feat: coffees deleted added

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
             const result = await coffeesCollection.insertOne(data);
             res.send(result);
         })
+        app.delete('/coffees/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await coffeesCollection.deleteOne(query);
+            res.send(result)
+        })
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
